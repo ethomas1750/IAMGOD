7b479dadd42d67203a58aa330edffa868e1bde1e
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-06-09 16:50:33 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose')
+const app = require('./app')
 
+mongoose
+    .connect("mongodb://localhost:27017/express-mongodb-intro")
+    .then(()=>{
+        app.listen(3000,()=>{
+            console.log("MONGO DB CONNECTED")
+            console.log("server started on port 3000")
+        })
+    }).catch((error)=>{
+        console.log(error)
+    })
